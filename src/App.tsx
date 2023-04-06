523644b4d2904f382a4b6fdd9e975cc520a7da2a
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-06 23:32:29 +0100

    chore: Main-Page-Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { List } from './pages/List'
 
 function App() {
   return (
     <div className="App">
       <h1>Questionnaire-star</h1>
+      <List />
     </div>
   )
 }
