2cd71f01c881c2cd58312c25f2ba8539de259bb9
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-06 23:38:40 +0100

    chore: mock-data-inital

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -2,7 +2,47 @@ import React, { FC, useState } from 'react'
 import styles from './List.module.scss'
 import { QuestionCard } from '../components/QuestionCard'
 
+const rawQuestionList = [
+  {
+    id: 'q1',
+    title: 'Question1',
+    isPublished: false,
+    isStar: false,
+    answerCount: 5,
+    createAt: '06/04 23:36',
+  },
+  {
+    id: 'q2',
+    title: 'Question2',
+    isPublished: true,
+    isStar: false,
+    answerCount: 5,
+    createAt: '05/04 23:36',
+  },
+  {
+    id: 'q3',
+    title: 'Question3',
+    isPublished: false,
+    isStar: false,
+    answerCount: 5,
+    createAt: '04/04 23:36',
+  },
+  {
+    id: 'q4',
+    title: 'Question4',
+    isPublished: true,
+    isStar: false,
+    answerCount: 5,
+    createAt: '03/04 23:36',
+  },
+]
+
 export const List: FC = () => {
+  {
+    /* Temporary Mock Data */
+  }
+  const [questionList, setQuestionList] = useState(rawQuestionList)
+
   return (
     <>
       <h1>List</h1>
