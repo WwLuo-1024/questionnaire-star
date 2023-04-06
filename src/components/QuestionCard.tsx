6a992503f281c37893ec926d27086e9ff42572db
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-06 23:32:14 +0100

    chore: Question-Card-Initial

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import styles from './QuestionCard.module.scss'
+
+export const QuestionCard: FC = () => {
+  return (
+    <>
+      <h2>Question Card Page</h2>
+    </>
+  )
+}
