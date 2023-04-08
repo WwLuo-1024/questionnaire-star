1b11e0da1a52c6abc0e6a2c0052307a589d80568
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-08 19:40:26 +0100

    chore: home-page-update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import 'antd/dist/reset.css'
-import { Button } from 'antd'
+import { Button, Typography } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+import styles from './Home.module.scss'
 
+const { Title, Paragraph } = Typography
 export const Home: FC = () => {
   const nav = useNavigate()
   function clickHandler() {
@@ -10,12 +13,19 @@ export const Home: FC = () => {
   }
 
   return (
-    <>
-      <p>Home</p>
-      <div>
-        <Button onClick={clickHandler}>Login</Button>
-        <Link to="/register">Register</Link>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>Questionnaire | Online Voting</Title>
+        <Paragraph>
+          A total of 100 have been created, 90 distribution questionnaires and 980 responses
+          received
+        </Paragraph>
+        <div>
+          <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+            Start
+          </Button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
