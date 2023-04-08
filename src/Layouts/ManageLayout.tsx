4cb91a8aa34ebfecc4c932c3c864017e143a9fa8
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-08 18:15:36 +0100

    chore: list-page-button

--- src/Layouts/ManageLayout.tsx
+++ src/Layouts/ManageLayout.tsx
@@ -1,19 +1,47 @@
 import React, { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import styles from './ManageLayout.module.scss'
+import { Button, Space, Divider } from 'antd'
+import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
 
 export const ManageLayout: FC = () => {
+  const nav = useNavigate()
+  const { pathname } = useLocation()
+
   return (
     <div className={styles['container']}>
       <div className={styles.left}>
-        <p>ManageLayout</p>
-        <button>Create Qustionaaire</button>
-        <br />
-        <a href="#">My Questionnaire</a>
-        <br />
-        <a href="#">Star Questionnaire</a>
-        <br />
-        <a href="#">Recycle Bin</a>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            Create Qustionaaire
+          </Button>
+          {/* style={{ borderTop: 'transparent' }} Delete line style */}
+          <Divider style={{ borderTop: 'transparent' }} />
+          <Button
+            type={pathname.startsWith('/manage/list') ? 'default' : 'text'}
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => nav('/manage/list')}
+          >
+            My Questionnaire
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/star') ? 'default' : 'text'}
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => nav('/manage/star')}
+          >
+            Starred Questionnaire
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/recycle') ? 'default' : 'text'}
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => nav('/manage/recycle')}
+          >
+            Recycle Bin
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
