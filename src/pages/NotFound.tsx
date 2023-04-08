ed097627c3d856b15ab58a0c3c2c71ed93afce1e
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-08 21:46:15 +0100

    chore: 404-not-found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,20 @@
 import React, { FC } from 'react'
+import { Result, Button } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { HOME_PATHNAME } from '../router'
 
 export const NotFound: FC = () => {
+  const nav = useNavigate()
   return (
-    <>
-      <h1>404 Page</h1>
-    </>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, The page you visited does not exist"
+      extra={
+        <Button type="primary" onClick={() => nav(HOME_PATHNAME)}>
+          Back Home
+        </Button>
+      }
+    ></Result>
   )
 }
