3e5b272add9a3361538d826eed1bcf42900288d8
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-11 22:11:12 +0100

    refactor: login-page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,91 @@
-import React, { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Register.module.scss'
+import { REGISTER_PATHNAME } from '../router'
+
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORD_KEY, password)
+}
+
+function deleteFromStorage() {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORD_KEY)
+}
+
+function getUserInfoFromStorage() {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PASSWORD_KEY),
+  }
+}
 
 export const Login: FC = () => {
-  const nav = useNavigate()
+  // const nav = useNavigate()
+  const { Title } = Typography
+  const [form] = Form.useForm() //Form hook
+
+  useEffect(() => {
+    const { username, password } = getUserInfoFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+
+  const onFinish = (values: any) => {
+    console.log(values)
+    const { username, password, remember } = values || {}
+
+    if (values.remember) {
+      rememberUser(username, password)
+    } else {
+      deleteFromStorage()
+    }
+  }
 
   return (
-    <>
-      <h1>Login Page</h1>
+    <div className={styles.container}>
       <div>
-        <button onClick={() => nav(-1)}>Back</button>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>Login</Title>
+        </Space>
       </div>
-    </>
+
+      <div>
+        <Form
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          form={form}
+        >
+          <Form.Item label="Username" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+            <Checkbox>Remember Me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+              <Link to={REGISTER_PATHNAME}>Register new user</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
