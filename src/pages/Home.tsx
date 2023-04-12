91510118c84527d21fe219ea3c88ca4bec15ca18
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-12 11:23:04 +0100

    test: axios-request-mockjs data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import 'antd/dist/reset.css'
 import { Button, Typography } from 'antd'
 import { MANAGE_INDEX_PATHNAME } from '../router'
 import styles from './Home.module.scss'
+import '../_mock/index'
+import axios from 'axios'
 
 const { Title, Paragraph } = Typography
 export const Home: FC = () => {
   const nav = useNavigate()
+
+  useEffect(() => {
+    // fetch('/api/test')
+    //   .then(res => res.json())
+    //   .then(data => console.log('fetch data', data))
+    //fetch cannot get mock.js
+
+    //axios uses XMLHttpRequest internally
+    axios.get('/api/test').then(res => console.log('axios data', res.data))
+  }, [])
+
   function clickHandler() {
     nav('/login')
   }
