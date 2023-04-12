1a48145c3f890c1c43488a5b31305c105ab35a13
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-12 18:54:37 +0100

    chore: load-question-data hook

---
+++ src/hooks/useLoadQuestionData.ts
@@ -0,0 +1,22 @@
+//useLoadQuestionData
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getQuestionService } from '../services/question'
+
+function useLoadQuestionData() {
+  const { id = '' } = useParams()
+  const [loading, setLoading] = useState(true)
+  const [questionData, setQuestionData] = useState({})
+
+  useEffect(() => {
+    async function fn() {
+      const data = await getQuestionService(id)
+      setQuestionData(data)
+      setLoading(false)
+    }
+  }, [])
+
+  return { loading, questionData }
+}
+
+export default useLoadQuestionData
