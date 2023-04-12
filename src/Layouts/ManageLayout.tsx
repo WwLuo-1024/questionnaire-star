92c8aacd8b20ebb2e196fecf5a54faf516358b01
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-12 18:55:28 +0100

    test: ajax request to get data

--- src/Layouts/ManageLayout.tsx
+++ src/Layouts/ManageLayout.tsx
@@ -1,18 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import styles from './ManageLayout.module.scss'
-import { Button, Space, Divider } from 'antd'
+import { Button, Space, Divider, message } from 'antd'
 import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
+import { createQuestionService } from '../services/question'
 
 export const ManageLayout: FC = () => {
   const nav = useNavigate()
   const { pathname } = useLocation()
 
+  //Set Loaindg to prevent multiple clicks
+  const [loading, setLoading] = useState(false)
+  async function handleCreateClick() {
+    setLoading(true)
+    const data = await createQuestionService()
+    const { id } = data || {}
+    setLoading(false)
+    if (id) {
+      nav(`/question/edit/${id}`)
+      message.success('Successfully Created')
+    }
+  }
+
   return (
     <div className={styles['container']}>
       <div className={styles.left}>
         <Space direction="vertical">
-          <Button type="primary" size="large" icon={<PlusOutlined />}>
+          <Button
+            type="primary"
+            size="large"
+            icon={<PlusOutlined />}
+            onClick={handleCreateClick}
+            disabled={loading}
+          >
             Create Qustionaaire
           </Button>
           {/* style={{ borderTop: 'transparent' }} Delete line style */}
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,11 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { getQuestionService } from '../../../services/question'
 
 export const Edit: FC = () => {
   const { id = '' } = useParams()
+  const [loading, setLoading] = useState(true)
+  const [questionData, setQuestionData] = useState({})
+  useEffect(() => {
+    {
+      /* useEffect中不能直接使用异步函数 */
+    }
+    async function fn() {
+      const data = await getQuestionService(id)
+      setQuestionData(data)
+      setLoading(false)
+    }
+    fn()
+  }, [])
   return (
-    <>
-      <h1>Edit {id}</h1>
-    </>
+    <div>
+      <p>Edit Page</p>
+      {loading ? <p>Loading</p> : <p>{JSON.stringify(questionData)}</p>}
+    </div>
   )
 }
