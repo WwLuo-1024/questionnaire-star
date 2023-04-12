084882d82d9fdf993e0de22b55f365bd93124c5f
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-12 20:47:18 +0100

    refactor: refact ajax by useRequest

--- src/hooks/useLoadQuestionData.ts
+++ src/hooks/useLoadQuestionData.ts
@@ -1,23 +1,35 @@
 //useLoadQuestionData
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 import { getQuestionService } from '../services/question'
+import { useRequest } from 'ahooks'
 
 function useLoadQuestionData() {
   const { id = '' } = useParams()
-  const [loading, setLoading] = useState(true)
-  const [questionData, setQuestionData] = useState({})
 
-  useEffect(() => {
-    async function fn() {
-      const data = await getQuestionService(id)
-      setQuestionData(data)
-      setLoading(false)
-    }
-    fn()
-  }, [])
+  {
+    /* AJax */
+  }
+  // const [loading, setLoading] = useState(true)
+  // const [questionData, setQuestionData] = useState({})
 
-  return { loading, questionData }
+  // useEffect(() => {
+  //   async function fn() {
+  //     const data = await getQuestionService(id)
+  //     setQuestionData(data)
+  //     setLoading(false)
+  //   }
+  //   fn()
+  // }, [])
+
+  // return { loading, questionData }
+  async function load() {
+    const data = await getQuestionService(id)
+    return data
+  }
+
+  const { loading, data, error } = useRequest(load)
+  return { loading, data, error }
 }
 
 export default useLoadQuestionData
