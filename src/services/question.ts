09a0c83d77720905d6fcc2c9d45f258d3fce0140
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-12 18:55:08 +0100

    chore: ajax request settinng

---
+++ src/services/question.ts
@@ -0,0 +1,15 @@
+import axios, { ResDataType } from './ajax'
+
+//Get signle questionnaire info
+export async function getQuestionService(id: string): Promise<ResDataType> {
+  const url = `/api/question/${id}`
+  const data = (await axios.get(url)) as ResDataType
+  return data
+}
+
+//Create Questionnaire
+export async function createQuestionService(): Promise<ResDataType> {
+  const url = '/api/question'
+  const data = (await axios.post(url)) as ResDataType
+  return data
+}
