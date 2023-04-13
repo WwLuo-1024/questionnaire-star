6ff17b3b90dc04d291f2357b868bbb1c27a1d61a
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-13 21:07:10 +0100

    feat: refresh page

--- src/hooks/useLoadQuestionListData.ts
+++ src/hooks/useLoadQuestionListData.ts
@@ -18,7 +18,7 @@ function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
   const [searchParams] = useSearchParams()
   //   console.log('keyword', searchParams.get('keyword'))
 
-  const { data, loading, error } = useRequest(
+  const { data, loading, error, refresh } = useRequest(
     async () => {
       const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
       const page = parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || '') || '' || 1
@@ -32,7 +32,7 @@ function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
     }
   )
 
-  return { data, loading, error }
+  return { data, loading, error, refresh }
 }
 
 export default useLoadQuestionListData
