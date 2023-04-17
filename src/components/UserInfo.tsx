f6a8945f0e4890e899f7fe6a653bcae61107843d
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-17 21:00:37 +0100

    chore: obtain user data by redux

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -2,15 +2,23 @@ import React, { FC } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { LOGIN_PATHNAME, REGISTER_PATHNAME } from '../router'
 import { getUserInfoService } from '../services/user'
-import { useRequest } from 'ahooks'
+// import { useRequest } from 'ahooks'
 import { UserOutlined } from '@ant-design/icons'
 import { Button, message } from 'antd'
 import { removeToken } from '../utils/user-token'
+import useGetUserInfo from '../hooks/useGetUserInfo'
+// import useGetUserInfo from '../hooks/useGetUserInfo'
+import { useDispatch } from 'react-redux'
+import { logoutReducer } from '../store/userReducer'
 
 export const UserInfo: FC = () => {
-  const { data } = useRequest(getUserInfoService)
-  const { username, nickname } = data || {}
+  // const { data } = useRequest(getUserInfoService)
+  // const { username, nickname } = data || {}
+  const { username, nickname } = useGetUserInfo()
+
+  const dispatch = useDispatch()
   const nav = useNavigate()
+
   const UserInfo = (
     <>
       <span style={{ color: '#e8e8e8' }}>
@@ -26,6 +34,7 @@ export const UserInfo: FC = () => {
   const Login = <Link to={REGISTER_PATHNAME}>Login/Register</Link>
 
   function logout() {
+    dispatch(logoutReducer()) //clear redux user data
     removeToken() // Clear the storage of token
     message.success('Successfully Log Out')
     nav(LOGIN_PATHNAME)
