9971798776b475d3b90c4b8d4828ce36474f9152
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-17 20:58:51 +0100

    chore: navPage(loaded or unloaded)

---
+++ src/hooks/useNavPage.ts
@@ -0,0 +1,35 @@
+import useGetUserInfo from './useGetUserInfo'
+import { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import {
+  LOGIN_PATHNAME,
+  MANAGE_INDEX_PATHNAME,
+  isLoginOrRegister,
+  isNoNeedUserInfo,
+} from '../router'
+
+function useNavPage(waitingUserData: boolean) {
+  const { username } = useGetUserInfo()
+  const { pathname } = useLocation()
+  const nav = useNavigate()
+
+  useEffect(() => {
+    if (waitingUserData) return
+
+    //Already login
+    if (username) {
+      if (isLoginOrRegister(pathname)) {
+        nav(MANAGE_INDEX_PATHNAME)
+      }
+      return
+    }
+    //Not Login yet and no need userInfo
+    if (isNoNeedUserInfo(pathname)) {
+      return
+    } else {
+      nav(LOGIN_PATHNAME)
+    }
+  }, [waitingUserData, username, pathname])
+}
+
+export default useNavPage
