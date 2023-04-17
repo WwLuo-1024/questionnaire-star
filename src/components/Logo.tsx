7f94e80614106261c3308444e235bcad6662f818
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-17 21:00:09 +0100

    feat: click logo to navigate

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,22 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import { Space, Typography } from 'antd'
 import { FormOutlined } from '@ant-design/icons'
 import styles from './Logo.module.scss'
 import { Link } from 'react-router-dom'
-
+import useGetUserInfo from '../hooks/useGetUserInfo'
+import { HOME_PATHNAME, MANAGE_INDEX_PATHNAME } from '../router'
 const { Title } = Typography
 
 export const Logo: FC = () => {
+  const { username } = useGetUserInfo()
+  const [pathname, setPathname] = useState(HOME_PATHNAME)
+
+  useEffect(() => {
+    if (username) {
+      setPathname(MANAGE_INDEX_PATHNAME)
+    }
+  }, [username])
+
   return (
     <div className={styles.container}>
       <Link to="./">
