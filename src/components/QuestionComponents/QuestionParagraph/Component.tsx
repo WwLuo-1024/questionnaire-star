454793b6562ff61048c0d835bd5a71385326348a
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-26 11:09:41 +0100

    feat: paragraph component - wrap

--- src/components/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/components/QuestionComponents/QuestionParagraph/Component.tsx
@@ -2,12 +2,22 @@ import React, { FC } from 'react'
 import { QuestionParagraphPropsType, QuestionParagraphDefaultType } from './interface'
 import { Typography } from 'antd'
 
+const { Paragraph } = Typography
+
 const Component: FC<QuestionParagraphPropsType> = (props: QuestionParagraphPropsType) => {
   const { text = '', isCenter = false } = { ...QuestionParagraphDefaultType, ...props }
-  const { Paragraph } = Typography
+
+  //Do not use dangerouslySetInnerHTML!
+  const textListr = text.split('\n') //example: ['hello', '123', '456']
+
   return (
     <Paragraph style={{ textAlign: isCenter ? 'center' : 'start', marginBottom: '0' }}>
-      {text}
+      {textListr.map((t, index) => (
+        <span key={index}>
+          {index > 0 && <br />}
+          {t}
+        </span>
+      ))}
     </Paragraph>
   )
 }
