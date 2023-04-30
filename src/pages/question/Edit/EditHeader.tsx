13b8a1cd6eb4635cf4f4ab702c0c0dadf6646cf8
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-04-30 23:18:41 +0100

    feat: edit-page auto-save

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,13 +1,91 @@
-import React, { FC } from 'react'
+import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import styles from './EditHeader.module.scss'
-import { Button, Typography, Space } from 'antd'
-import { LeftOutlined } from '@ant-design/icons'
-import { useNavigate } from 'react-router-dom'
+import { Button, Typography, Space, Input } from 'antd'
+import { EditOutlined, LeftOutlined, LoadingOutlined } from '@ant-design/icons'
+import { useNavigate, useParams } from 'react-router-dom'
 import EditToolBar from './EditToolBar'
+import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import { changePageTitle } from '../../../store/pageInfoReducer'
+import { useDispatch } from 'react-redux'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { updateQuestionService } from '../../../services/question'
+import { useRequest, useKeyPress, useDebounceEffect } from 'ahooks'
 
+const { Title } = Typography
+
+//Display and modify title
+const TitleElem: FC = () => {
+  const { title } = useGetPageInfo()
+  const [editState, setEditState] = useState(false)
+  const dispatch = useDispatch()
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const newTitle = event.target.value.trim()
+    if (!newTitle) return
+    dispatch(changePageTitle(newTitle))
+  }
+
+  if (editState) {
+    return (
+      <Input
+        value={title}
+        onPressEnter={() => setEditState(false)}
+        onBlur={() => setEditState(false)}
+        onChange={handleChange}
+      />
+    )
+  }
+
+  return (
+    <Space>
+      <Title>{title}</Title>
+      <Button icon={<EditOutlined />} type="text" onClick={() => setEditState(true)} />
+    </Space>
+  )
+}
+
+const SaveButton: FC = () => {
+  //id+ pageInfo + componentList
+  const { componentList } = useGetComponentInfo()
+  const pageInfo = useGetPageInfo()
+  const { id } = useParams()
+
+  const { loading, run: save } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(id, { ...pageInfo, componentList })
+    },
+    { manual: true }
+  ) //手动-需要点击按钮执行
+
+  //Shortcut keys
+  useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+    event.preventDefault()
+    if (!loading) save()
+  })
+
+  //Auto Save (不是定期保存，不是定时器)
+  useDebounceEffect(
+    //和useEffect有同样的功能和写法 多一个参数添加wait 为防抖机制，防止多次更新
+    () => {
+      save()
+    },
+    [componentList, pageInfo],
+    {
+      wait: 1000,
+    }
+  )
+
+  return (
+    <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined /> : null}>
+      Save
+    </Button>
+  )
+}
+
+//Edit page Header
 const EditHeader: FC = () => {
   const nav = useNavigate()
-  const { Title } = Typography
 
   return (
     <div className={styles['header-wrapper']}>
@@ -18,7 +96,9 @@ const EditHeader: FC = () => {
               Back
             </Button>
 
-            <Title>Questionnaire Title</Title>
+            <Title>
+              <TitleElem />
+            </Title>
           </Space>
         </div>
         <div className={styles.main}>
@@ -26,7 +106,7 @@ const EditHeader: FC = () => {
         </div>
         <div className={styles.right}>
           <Space>
-            <Button>Save</Button>
+            <SaveButton />
             <Button type="primary">Post</Button>
           </Space>
         </div>
