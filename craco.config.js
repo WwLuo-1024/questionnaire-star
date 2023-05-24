777f8827efd05146927b931ddc23338c4db5ea4c
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-05-24 17:26:41 +0100

    perf: analyzing package capacity and Splitting public code and utilising caching

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,36 @@
 module.exports = {
+  webpack: {
+    configure(webpackConfig) {
+      if (webpackConfig.mode === 'production') {
+        //抽离公共代码，只在生产环境
+        if (webpackConfig.optimization == null) {
+          webpackConfig.optimization = {}
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all',
+          cacheGroups: {
+            antd: {
+              name: 'antd-chunk',
+              test: /antd/, //匹配antd的都适用该规则
+              priority: 100,
+            },
+            reactDom: {
+              name: 'reactDom-chunk',
+              test: /react-dom/,
+              priority: 99,
+            },
+            vendors: {
+              name: 'vendors-chunk',
+              test: /node_modules/,
+              priority: 98,
+            },
+          },
+        }
+      }
+      return webpackConfig
+    },
+  },
+
   //common js
   devServer: {
     port: 8000, //business clinet
